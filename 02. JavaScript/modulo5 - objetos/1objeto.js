d784fbb918fded869d1a8bcd236c7584c07d1e28
Rafaela Nascimento <rafaelanascm@gmail.com>
2023-03-17 13:26:46 -0300

    Update 1objeto.js
    Adicionando modulo objeto

---
+++ 02. JavaScript/modulo5 - objetos/1objeto.js
@@ -0,0 +1,9 @@
+//ex de objeto 
+var dadosPessoais = {
+  nome: 'Rafaela',
+  idade: 22,
+  mulher: true
+}
+
+// para acessar a propriedade do objeto basta inserir "."
+console.log(dadosPessoais.nome)
